5be76f81785da5c119f5dce7d038ca093e9814d4
Ryan Meza <ryanmeza116@gmail.com>
2023-01-24 13:11:08 -0500

    Python Functions

--- Python/functions_basic.py
+++ Python/functions_basic.py
@@ -11,5 +11,33 @@ def print_and_return(list):
 
 print(print_and_return([1,2]))
 
+def first_plus_length(list):
+    return list[0] + len(list)
+
+print(first_plus_length([1,2,3,4,5]))
+
+def values_greater_than_second(list):
+    new_list = []
+    for i in list:
+        if(i > list[1]):
+            new_list.append(i)
+    print(new_list)
+    return new_list
+
+values_greater_than_second([1,2,3,4,5,6,7,8])
+
+def this_length_that_value(size, value):
+    new_list = []
+    for i in range(size):
+        new_list.append(value)
+    return new_list
+
+print(this_length_that_value(5,9))
+
+
+
+
+
+
 
 
